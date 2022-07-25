fec420a43f8e16466573cff916077dcddf2b4314
Eric Coleman <eric@epiccoleman.com>
2022-07-25 13:47:39 -0400

    add docstrings to FileUtils

--- lib/aoc_utils/file_utils.ex
+++ lib/aoc_utils/file_utils.ex
@@ -1,9 +1,19 @@
 defmodule AocUtils.FileUtils do
+  @moduledoc """
+  Defines some utility functions for processing puzzle input files.
+  """
+
+  @doc """
+  Reads an input file and returns its contents as a list of strings.
+  """
   def get_file_as_strings(file_path) do
     File.read!(file_path)
     |> String.split("\n")
   end
 
+  @doc """
+  Reads an input file and returns its contents as a list of integers.
+  """
   def get_file_as_integers(file_path) do
     get_file_as_strings(file_path)
     |> Enum.map(&String.to_integer/1)
