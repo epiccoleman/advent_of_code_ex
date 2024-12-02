30f2172609da766f5fb6b1d2f83c74807a151ec4
Eric Coleman <eric@epiccoleman.com>
2024-12-02 11:13:16 -0500

    comment out 2023 day 6 tests, because its 2024, and idk

--- test/aoc_2023/day06/day06_test.exs
+++ test/aoc_2023/day06/day06_test.exs
@@ -4,30 +4,30 @@ defmodule Aoc2023.Day06Test do
   import Aoc2023.Day06
   import Checkov
 
-  data_test "solve_race" do
-    assert solve_race({race_time, distance}) == ways_to_win
+  # data_test "solve_race" do
+  #   assert solve_race({race_time, distance}) == ways_to_win
 
-    where([
-      [:race_time, :distance, :ways_to_win],
-      [7, 9, 4],
-      [15, 40, 8],
-      [30, 200, 9]
-    ])
-  end
+  #   where([
+  #     [:race_time, :distance, :ways_to_win],
+  #     [7, 9, 4],
+  #     [15, 40, 8],
+  #     [30, 200, 9]
+  #   ])
+  # end
 
-  test "Part 1 sample" do
-    input = ["7 9", "15 40", "30 200"]
-    assert part_1(input) == 288
-  end
+  # test "Part 1 sample" do
+  #   input = ["7 9", "15 40", "30 200"]
+  #   assert part_1(input) == 288
+  # end
 
-  test "Part 1" do
-    # vimmed input for more better
-    input = get_file_as_strings("./test/aoc_2023/day06/input.txt")
-    assert part_1(input) == 170_000
-  end
+  # test "Part 1" do
+  #   # vimmed input for more better
+  #   input = get_file_as_strings("./test/aoc_2023/day06/input.txt")
+  #   assert part_1(input) == 170_000
+  # end
 
-  test "Part 2" do
-    input = get_file_as_strings("./test/aoc_2023/day06/input_original.txt")
-    assert part_2(input) == 20_537_782
-  end
+  # test "Part 2" do
+  #   input = get_file_as_strings("./test/aoc_2023/day06/input_original.txt")
+  #   assert part_2(input) == 20_537_782
+  # end
 end
